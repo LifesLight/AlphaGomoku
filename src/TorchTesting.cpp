0a143c36085cfc0e225fb23a634f48fcd56a1a8e
Alexander Kurtz <alexkurtz@web.de>
2023-10-05 19:00:50 +0200

    Added prediction code to TorchTesting.cpp

--- src/TorchTesting.cpp
+++ src/TorchTesting.cpp
@@ -1,9 +1,10 @@
-#include <torch/script.h> // One-stop header.
+#include <torch/script.h>
 
 #include <iostream>
 #include <memory>
 
 int main(int argc, const char* argv[]) {
+  
   if (argc != 2) {
     std::cerr << "usage: example-app <path-to-exported-script-module>\n";
     return -1;
@@ -12,8 +13,8 @@ int main(int argc, const char* argv[]) {
 
   torch::jit::script::Module module;
   try {
-    // Deserialize the ScriptModule from a file using torch::jit::load().
     module = torch::jit::load(argv[1]);
+    module.to(torch::kCUDA);
   }
   catch (const c10::Error& e) {
     std::cerr << "error loading the model\n" << e.what() << std::endl;
@@ -21,4 +22,13 @@ int main(int argc, const char* argv[]) {
   }
 
   std::cout << "Loaded Model.\n";
+  
+  torch::Tensor tensor = torch::zeros({1, 15, 15, 15}, torch::kFloat32).to(torch::kCUDA);
+  tensor[0][0] = 1.0f;
+  torch::Tensor result = module.forward({tensor}).toTensor().view({225});
+  result = torch::softmax(result, -1);
+
+  int index = result.argmax(-1).item<int>();
+  std::cout << "Prediction: " << index << std::endl;
+  return 0;
 }
