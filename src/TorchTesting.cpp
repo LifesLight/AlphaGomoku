bb8fc27e92f3c14447dce2e37cef2170b7a8b136
Alexander Kurtz <alexkurtz@web.de>
2023-10-08 18:30:59 +0200

    Moved to Pytorch 2.1.0.

--- src/TorchTesting.cpp
+++ src/TorchTesting.cpp
@@ -1,10 +1,16 @@
 #include <torch/script.h>
+#include <torch/torch.h>
 
 #include <iostream>
 #include <memory>
 
 int main(int argc, const char* argv[]) {
   
+  std::cout << "PyTorch version: "
+    << TORCH_VERSION_MAJOR << "."
+    << TORCH_VERSION_MINOR << "."
+    << TORCH_VERSION_PATCH << std::endl;
+
   if (argc != 2) {
     std::cerr << "usage: example-app <path-to-exported-script-module>\n";
     return -1;
@@ -23,12 +29,12 @@ int main(int argc, const char* argv[]) {
 
   std::cout << "Loaded Model.\n";
   
-  torch::Tensor tensor = torch::zeros({1, 15, 15, 15}, torch::kFloat32).to(torch::kCUDA);
-  tensor[0][0] = 1.0f;
+  torch::Tensor tensor = torch::zeros({1, 11, 15, 15}, torch::kFloat32).to(torch::kCUDA);
+  tensor[0][0] = 0.0f;
   torch::Tensor result = module.forward({tensor}).toTensor().view({225});
   result = torch::softmax(result, -1);
 
   int index = result.argmax(-1).item<int>();
-  std::cout << "Prediction: " << index << std::endl;
+  std::cout << "Prediction: " << index << "(" << index / 15 << "," << index % 15 << ")" << std::endl;
   return 0;
 }
